3821abffea29563a768095a5950f16c437e0ece3
Practically Predictable <practicallypredictable@practicallypredictable.com>
2019-02-14 22:07:53 -0500

    changed mapping to return iterators

--- src/litecore/mapping.py
+++ src/litecore/mapping.py
@@ -1,5 +1,10 @@
 """Convenience functions for mappings.
 
+Unless noted otherwise, each function returns an iterator of (key, value)
+tuples in the same order as iteration over the keys of the original mapping
+argument. These tuples may then be used to instantiate whatever type of mapping
+the user desires.
+
 """
 __all__ = [
     'is_one_to_one',
@@ -10,15 +15,14 @@ __all__ = [
     'inverted',
 ]
 
-import collections
-
 from typing import (
     Any,
     Callable,
     Iterable,
+    Iterator,
     Mapping,
     Optional,
-    OrderedDict,
+    Tuple,
     TypeVar,
 )
 
@@ -64,7 +68,7 @@ def keeping_keys(
         *,
         keys: Optional[Iterable[KT]] = None,
         where: Optional[Callable[[KT], bool]] = None,
-) -> OrderedDict[KT, VT]:
+) -> Iterator[Tuple[KT, VT]]:
     """Return mapping keeping items with keys matching conditions.
 
     Args:
@@ -84,23 +88,22 @@ def keeping_keys(
     True will be kept.
 
     >>> data = {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> keeping_keys(data)
+    >>> dict(keeping_keys(data))
     {}
-    >>> keeping_keys(data, keys=data.keys())
+    >>> dict(keeping_keys(data, keys=data.keys()))
     {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> keeping_keys(data, keys=['b', 'e'])
+    >>> dict(keeping_keys(data, keys=['b', 'e']))
     {'b': 3, 'e': 4}
-    >>> keeping_keys(data, where=lambda x: x.lower() in 'aeiou')
+    >>> dict(keeping_keys(data, where=lambda x: x.lower() in 'aeiou'))
     {'a': 1, 'A': 2, 'e': 4, 'i': 5}
-    >>> keeping_keys(data, where=lambda x: x.lower() in 'aeiou', keys=['b'])
+    >>> dict(keeping_keys(data, where=lambda x: x.lower() in 'aeiou', keys=['b']))
     {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> keeping_keys(collections.OrderedDict([('a', 1), ('b', 2)]), keys=['b'])
-    OrderedDict([('b', 2)])
 
     """
     keep = _meets_condition(iterable=keys, callable=where)
-    kept = ((k, v) for k, v in mapping.items() if keep(k))
-    return type(mapping)(kept)
+    for k, v in mapping.items():
+        if keep(k):
+            yield (k, v)
 
 
 def dropping_keys(
@@ -108,7 +111,7 @@ def dropping_keys(
         *,
         keys: Optional[Iterable[KT]] = None,
         where: Optional[Callable[[KT], bool]] = None,
-) -> OrderedDict[KT, VT]:
+) -> Iterator[Tuple[KT, VT]]:
     """Return mapping dropping items with keys matching conditions.
 
     Args:
@@ -128,23 +131,22 @@ def dropping_keys(
     True will be dropped.
 
     >>> data = {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> dropping_keys(data)
+    >>> dict(dropping_keys(data))
     {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> dropping_keys(data, keys=data.keys())
+    >>> dict(dropping_keys(data, keys=data.keys()))
     {}
-    >>> dropping_keys(data, keys=['b', 'e'])
+    >>> dict(dropping_keys(data, keys=['b', 'e']))
     {'a': 1, 'A': 2, 'i': 5}
-    >>> dropping_keys(data, where=lambda x: x.lower() in 'aeiou')
+    >>> dict(dropping_keys(data, where=lambda x: x.lower() in 'aeiou'))
     {'b': 3}
-    >>> dropping_keys(data, where=lambda x: x.lower() in 'b', keys=['e', 'i'])
+    >>> dict(dropping_keys(data, where=lambda x: x.lower() in 'b', keys=['e', 'i']))
     {'a': 1, 'A': 2}
-    >>> dropping_keys(collections.OrderedDict([('a', 1), ('b', 2)]), keys=['b'])
-    OrderedDict([('a', 1)])
 
     """
     drop = _meets_condition(iterable=keys, callable=where)
-    kept = ((k, v) for k, v in mapping.items() if not drop(k))
-    return type(mapping)(kept)
+    for k, v in mapping.items():
+        if not drop(k):
+            yield (k, v)
 
 
 def keeping_values(
@@ -152,7 +154,7 @@ def keeping_values(
         *,
         values: Optional[Iterable[VT]] = None,
         where: Optional[Callable[[VT], bool]] = None,
-) -> OrderedDict[KT, VT]:
+) -> Iterator[Tuple[KT, VT]]:
     """Return mapping keeping items with values matching conditions.
 
     Args:
@@ -172,23 +174,22 @@ def keeping_values(
     True will be kept.
 
     >>> data = {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> keeping_values(data)
+    >>> dict(keeping_values(data))
     {}
-    >>> keeping_values(data, values=data.values())
+    >>> dict(keeping_values(data, values=data.values()))
     {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> keeping_values(data, values=[3, 4])
+    >>> dict(keeping_values(data, values=[3, 4]))
     {'b': 3, 'e': 4}
-    >>> keeping_values(data, where=lambda x: x != 3)
+    >>> dict(keeping_values(data, where=lambda x: x != 3))
     {'a': 1, 'A': 2, 'e': 4, 'i': 5}
-    >>> keeping_values(data, where=lambda x: x != 2, values=[2])
+    >>> dict(keeping_values(data, where=lambda x: x != 2, values=[2]))
     {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> keeping_values(collections.OrderedDict([('a', 1), ('b', 2)]), values=[2])
-    OrderedDict([('b', 2)])
 
     """
     keep = _meets_condition(iterable=values, callable=where)
-    kept = ((k, v) for k, v in mapping.items() if keep(v))
-    return type(mapping)(kept)
+    for k, v in mapping.items():
+        if keep(v):
+            yield (k, v)
 
 
 def dropping_values(
@@ -196,7 +197,7 @@ def dropping_values(
         *,
         values: Optional[Iterable[VT]] = None,
         where: Optional[Callable[[VT], bool]] = None,
-) -> OrderedDict[KT, VT]:
+) -> Iterator[Tuple[KT, VT]]:
     """Return mapping dropping items with values matching conditions.
 
     Args:
@@ -216,26 +217,25 @@ def dropping_values(
     True will be dropped.
 
     >>> data = {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> dropping_values(data)
+    >>> dict(dropping_values(data))
     {'a': 1, 'A': 2, 'b': 3, 'e': 4, 'i': 5}
-    >>> dropping_values(data, values=data.values())
+    >>> dict(dropping_values(data, values=data.values()))
     {}
-    >>> dropping_values(data, values=[3, 4])
+    >>> dict(dropping_values(data, values=[3, 4]))
     {'a': 1, 'A': 2, 'i': 5}
-    >>> dropping_values(data, where=lambda x: x != 3)
+    >>> dict(dropping_values(data, where=lambda x: x != 3))
     {'b': 3}
-    >>> dropping_values(data, where=lambda x: x >= 4, values=[3])
+    >>> dict(dropping_values(data, where=lambda x: x >= 4, values=[3]))
     {'a': 1, 'A': 2}
-    >>> dropping_values(collections.OrderedDict([('a', 1), ('b', 2)]), values=[2])
-    OrderedDict([('a', 1)])
 
     """
     drop = _meets_condition(iterable=values, callable=where)
-    kept = ((k, v) for k, v in mapping.items() if not drop(v))
-    return type(mapping)(kept)
+    for k, v in mapping.items():
+        if not drop(v):
+            yield (k, v)
 
 
-def inverted(mapping: Mapping[KT, VT]) -> OrderedDict[VT, KT]:
+def inverted(mapping: Mapping[KT, VT]) -> Iterator[Tuple[KT, VT]]:
     """Returns a mapping inverting the values and keys of a given mapping.
 
     Args:
@@ -247,19 +247,17 @@ def inverted(mapping: Mapping[KT, VT]) -> OrderedDict[VT, KT]:
     original mapping. The values of the returned mapping will be the keys of
     the orignal mapping.
 
-    The items of the returned mapping will in the same order as iterating
-    over the original mapping's keys.
-
     If the original mapping is not 1-to-1 (i.e., there are duplicate values),
     the last observed item iterating through the original mapping's keys will
     be the only item retained in the returned mapping.
 
     >>> original = {'a': 2, 'b': -1, 'c': 0, 'd': -3}
-    >>> inverted(original)
-    OrderedDict([(2, 'a'), (-1, 'b'), (0, 'c'), (-3, 'd')])
+    >>> dict(inverted(original))
+    {2: 'a', -1: 'b', 0: 'c', -3: 'd'}
     >>> one_to_many = {'alice': 10, 'bob': 20, 'charlie': 10, 'david': 20}
-    >>> inverted(one_to_many)
-    OrderedDict([(10, 'charlie'), (20, 'david')])
+    >>> dict(inverted(one_to_many))
+    {10: 'charlie', 20: 'david'}
 
     """
-    return collections.OrderedDict((v, k) for k, v in mapping.items())
+    for k, v in mapping.items():
+        yield (v, k)
